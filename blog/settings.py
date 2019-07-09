9f687436de37435dabff9dfc168eed324b324cef
sellerselijahsellers@gmail.com <sellerselijahsellers@gmail.com>
2019-07-08 23:21:09 -0500

    FIIX

--- blog/settings.py
+++ blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#)!6z-+@ah$z&%0py(v2rg-ok4mes$v5_=p_4hmb5!sc9af4p-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://almostgone.online','https://leenah-elijah.herokuapp.com/',"*"]
+ALLOWED_HOSTS = []
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 # Application definition
